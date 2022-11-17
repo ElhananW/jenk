df64f2a2d0a4c74d1ad2b6c4984a6b3dd351e76a
ElhananW <101626301+ElhananW@users.noreply.github.com>
2022-11-17 10:08:58 +0200

    check

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("hi!")
+#!/usr/bin/python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'hello world from first version of attendance app'
+
+
+app.run(host='0.0.0.0', port=5000)
+#
