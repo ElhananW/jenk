b3948c98c1a624221fc162447f7ecfdeab6a2ab9
ElhananW <101626301+ElhananW@users.noreply.github.com>
2022-11-20 22:57:00 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,53 +1,32 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 HOME_DIR="/home/ec2-user"
 WORKSPACE="/var/lib/jenkins/workspace/proj"
-DIR_NAME=proj
-machine=$1
+MACHINE=$1
 
-##########################
-#TODO handle vars names and messages!!!!!!
-##########################
+if [ "$#" -ne 1 ]; then
+    echo "You entered more/less than 1 argument"
+	exit 1
+fi
 
-usage(){
-  echo "Usage: ./deploy.sh [test | prod]"
-  exit 1
-}
+if [ "$MACHINE" == "test" ];
+then
+	cd /home/ec2-user/Proj/
+	sudo docker-compose up --no-build -d
+	sleep 5
+	./test.sh
+	fi
+fi
 
-execute_test_case(){
-  echo "Now deploying to test machine."
-  scp -o StrictHostKeyChecking=no docker-compose.yml test.sh ec2-user@$machine:$HOME_DIR/
-  ssh -o StrictHostKeyChecking=no ec2-user@$machine 'docker-compose up --no-build -d'
-  sleep 3
-
-}
-
-
-execute_prod_case(){
-  echo "Now deploying to prod machine."
-  #TODO: mkdir and validate if there is exiting dir
+if [ "$MACHINE" == "prod" ];
+then
+	if [ $MY_IP == $DEPLOY_IP ];
+	then
 	cd /home/ec2-user/BynetFinalProject/
 	sudo docker pull avielderhy/finalproject:latest
-	sleep 3
+	sleep 1
 	docker-compose up --no-build -d
-}
+	fi
+fi
 
-
-main(){
-  if [ "$#" -ne 1 ];
-  then
-    echo "You entered more/less than 1 argument"
-  	exit 1
-  elif [ "$machine" == "test" ];
-  then
-    execute_test_case
-  elif [ "$machine" == "prod" ];
-  then
-    execute_prod_case
-  else
-    echo "Bad argument $machine"
-    usage
-  fi
-
-  echo "The application was successfully deployed on $machine machine "
-}
+echo "Deployed to $MACHINE machine successfully"
