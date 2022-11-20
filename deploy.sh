ba5549a1509f566e2fa796c58b672a936c31ca09
ElhananW <101626301+ElhananW@users.noreply.github.com>
2022-11-20 17:23:07 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-HOME_DIR = "/home/ec2-user"
-WORKSPACE = "/var/lib/jenkins/workspace/BynetFinalProject"
-#TEST_IP = "54.83.91.121"
-#DEPLOY_IP = "18.208.110.157"
-#MY_IP = "$(dig +short myip.opendns.com @resolver1.opendns.com)"
-machine = $1
+HOME_DIR="/home/ec2-user"
+WORKSPACE="/var/lib/jenkins/workspace/proj"
+DIR_NAME=proj
+machine=$1
 
 ##########################
-#TODO handle vars names!!!!!!
+#TODO handle vars names and messages!!!!!!
 ##########################
 
 usage(){
@@ -18,27 +16,19 @@ usage(){
 
 execute_test_case(){
   echo "Now deploying to test machine."
-#  ssh ec2-user@$machine_name "mkdir -p $HOME_DIR/$DIR_NAME"    sudo docker-compose up --no-build -d
-    sleep 5
-    status=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:5000"`
-    if [ "$status" == "200" ];
-      then
-        echo "test PASSED successfully."
-      else
-        echo "Test Failed"
-    fi
+  ssh -o StrictHostKeyChecking=no ec2-user@$machine 'docker-compose up --no-build -d'
+  sleep 3
+
 }
 
 
 execute_prod_case(){
   echo "Now deploying to prod machine."
-  if [ $MY_IP == $DEPLOY_IP ];
-	then
-#	cd /home/ec2-user/BynetFinalProject/
-	#sudo docker pull avielderhy/finalproject:latest
-	sleep 1
+  #TODO: mkdir and validate if there is exiting dir
+	cd /home/ec2-user/BynetFinalProject/
+	sudo docker pull avielderhy/finalproject:latest
+	sleep 3
 	docker-compose up --no-build -d
-	fi
 }
 
 
@@ -47,10 +37,10 @@ main(){
   then
     echo "You entered more/less than 1 argument"
   	exit 1
-  elif ["$machine" == "test"];
+  elif [ "$machine" == "test" ];
   then
     execute_test_case
-  elif ["$machine" == "prod"];
+  elif [ "$machine" == "prod" ];
   then
     execute_prod_case
   else
