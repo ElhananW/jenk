633e4b5fe77cb34db2fd4ea94b687b8a53d75bea
ElhananW <101626301+ElhananW@users.noreply.github.com>
2022-11-21 10:41:36 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-                 ### USAGE ###
-### THIS IS A BASH SCRIPT TO DEPLOY MY WEB APP
-### Accepts one argument, the host name machine.######
-
-#VARS#
 HOME_DIR=/home/ec2-user
 PIPLINE_WORKSPACE=/var/lib/jenkins/workspace/proj@2
 DIR_NAME=proj
@@ -18,7 +13,6 @@ fi
 
 clean_previous_builds(){
     # remove all images and containers
-    echo "Remove all containers and images"
     ssh ec2-user@$machine_name "cd /home/ec2-user/$DIR_NAME; docker-compose down -v"
     ssh ec2-user@$machine_name "cd /home/ec2-user/$DIR_NAME; docker system prune -a --volumes -f"
     }
@@ -37,24 +31,17 @@ check_args() {
 
 main (){
     # Connecting to machine and create new dir
-    echo "Connecting to $machine_name"
     ssh ec2-user@$machine_name "mkdir -p $HOME_DIR/$DIR_NAME"
     # Copy docker compose to machine
-    echo "Copy docker-compose to $machine_name"
     scp $PIPLINE_WORKSPACE/docker-compose.yml ec2-user@$machine_name:$HOME_DIR/$DIR_NAME
     clean_previeous_builds
-    echo "run docker compose up"
     ssh ec2-user@$machine_name "cd /home/ec2-user/$DIR_NAME; docker-compose up -d --no-build"
-
-    ## DO TESTS IF YOUR IN TEST MACHINE ##
     if [ $machine_name == "test" ]; then 
         # copy script to test machine 
-        echo "copy test.sh to TEST machine"
         scp /var/lib/jenkins/workspace/final-project/tests/test.sh ec2-user@$machine_name:$HOME_DIR/$DIR_NAME
         # give permissions to test.sh file
         ssh ec2-user@test "chmod u+x $HOME_DIR/$DIR_NAME/test.sh"
         # RUN TESTS
-        echo "Running tests"
         ssh ec2-user@test "cd /home/ec2-user/$DIR_NAME; ./test.sh " 
     fi   
 
