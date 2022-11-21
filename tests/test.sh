fca3852e4776974d9759e9a1af17f872facbdd99
ElhananW <101626301+ElhananW@users.noreply.github.com>
2022-11-21 11:21:14 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 status=`curl --write-out "%{http_code}\n" --silent --output /dev/null "http://127.0.0.1:5000"`
-    if [ "$status" == "200" ];
+    if [ "$status" == "500" ];
       then
         echo "test PASSED successfully."
       else
